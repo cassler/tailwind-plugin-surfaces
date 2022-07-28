6a777c1a90ca7f9e9ef77db327b5ddfba65b604d
Darin Cassler <darin@cassler.net>
2022-07-28 12:31:57 -0500

    revert to js

---
+++ src/index.js
@@ -0,0 +1,95 @@
+const plugin = require('tailwindcss/plugin');
+
+module.exports = plugin(( { matchUtilities, theme }) => {
+
+  /**
+   * This plugin provides a box-* utility class. This allows
+   * inheritence of default color palettes for background and test respectively.
+   *
+   * Ex: `.box-red-200` will have a bg of red-200, black titles, and red-900 body.
+   * Ex: `.box-blue-700` will have a bg of blue-700, white titles and blue-50 body.
+   */
+
+function getRGBA(color) {
+  const hex = color.replace('#', '');
+  const bits = hex.length / 3;
+  const factor = bits === 1 ? 17 : 1;
+  const [R,G,B] = [0,1,2].map(x => {
+    let localHex = hex.slice(bits*x, bits*(x+1));
+    return parseInt(localHex, 16) * factor
+  })
+  const brightness = ((R * 299) + (G * 587) + (B * 114)) / 1000;
+  const isBright = brightness > 155
+  return { R,G,B,brightness,isBright }
+}
+
+function toRGBA(color, alpha = 1) {
+  const { R,G,B } = getRGBA(color)
+  return `rgba(${R},${G},${B},${alpha})`
+}
+
+function hexIsLight(color) {
+  const { isBright } = getRGBA(color);
+  return isBright;
+}
+
+  function getColorScale(hue) {
+    return theme(`colors.${hue.toString()}`);
+  }
+
+  let btnBase = {
+    letterSpacing: '-0.01333em',
+    fontSize: '12px',
+    padding: theme('spacing.1'),
+    paddingLeft: theme('spacing.2'),
+    paddingRight: theme('spacing.2'),
+    borderRadius: theme('borderRadius.sm'),
+    backgroundColor: 'transparent',
+    border: '0px solid var(--border-color)',
+    transition: 'all 0.2s ease-in-out',
+    color: 'var(--txt-color)',
+  };
+
+  return !!matchUtilities && Object.keys(theme('colors')).map(hue => {
+    const colors = getColorScale(hue);
+    return matchUtilities({
+
+      [`box-${hue}`]: (value) => {
+        let isLight = hexIsLight(value);
+        return {
+          '--tw-ring-color': isLight ? toRGBA(colors['300'], '0.5') : toRGBA(colors['900'], '0.5'),
+          '--tw-ring-opacity': '0.5',
+          '--border-color': toRGBA(isLight ? colors['300'] : colors['500'], '0.8'),
+          backgroundColor: value,
+          color: isLight ? colors['900'] : colors['50'],
+          borderColor: isLight ? colors['500'] : colors['300'],
+          'h1,h2,h3,h4,h5,h6': {
+            fontWeight: '600',
+            color: isLight ? colors['900'] : colors['50'],
+          },
+          'small,caption,footer': {
+            color: isLight ? colors['700'] : colors['300'],
+          },
+          'button': {
+            '--border-color': isLight ? toRGBA(colors[300], '0.5') : toRGBA(colors[900], '0.5'),
+            '--bg-color': toRGBA(`${isLight ? colors[700] : colors[50]}`, '0.75'),
+            '--bg-hover': toRGBA(`${isLight ? colors[700] : colors[50]}`, '0.1'),
+            '--txt-color': isLight ? colors[700] : colors[50],
+            ...btnBase,
+            '&:hover': {
+              backgroundColor: 'var(--bg-hover)',
+            },
+            '&[type="submit"]': {
+              color: isLight ? colors[50] : colors[900],
+              backgroundColor: 'var(--bg-color)',
+            },
+          },
+        };
+      },
+    }, {
+      values: getColorScale(hue),
+      type: 'color',
+    });
+  });
+});
+
