af84c1c9ec2d0a75c2cbf84455a56b5eed068717
Darin Cassler <darin@cassler.net>
2022-07-29 10:13:14 -0500

    chore: 0.1.15

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import type { CSSRuleObject } from 'tailwindcss/types/config';
 import type { DefaultColors } from 'tailwindcss/types/generated/colors';
 
 type DefaultColorScales = Omit<DefaultColors, 'inherit' | 'current' | 'transparent' | 'black' | 'white'>;
-type DefaultColorName = keyof DefaultColorScales;
+type ColorGetterFn = <T extends keyof DefaultColorScales>(hue: T) => DefaultColorScales[T];
 
 export const getRGBA = (color:string) => {
   const hex = color.replace('#', '');
@@ -16,83 +16,79 @@ export const getRGBA = (color:string) => {
   const brightness = ((R * 299) + (G * 587) + (B * 114)) / 1000;
   const isBright = brightness > 155;
   return { R, G, B, brightness, isBright };
-};
+}
 
 export const toRGBA = (color:string, alpha:string | number = 1):string => {
   const { R, G, B } = getRGBA(color);
   return `rgba(${R},${G},${B},${alpha})`;
-};
+}
 
 export const hexIsLight = (color:string):boolean => {
   const { isBright } = getRGBA(color);
   return isBright;
-};
+}
 
-export const getVar = <
-  P extends keyof DefaultColorScales,
-  T extends keyof DefaultColorScales[P]
->([low, hi, ref]: [T,T,string], h: P, themeFn: (arg0: string) => DefaultColorScales[P]) => {
-  return themeFn(`colors.${h}`)[hexIsLight(ref) ? low : hi];
-};
 
 const surfaces = plugin(( { matchUtilities, theme } ) => {
-  // check that this version of Tailwind supports our plugin.
-  if (!matchUtilities) return;
-  // get all of the color groupings in our current theme.
-  const shades = Object.keys(theme('colors')) as DefaultColorName[];
-  // stashing this here as a way to keep global CSS consistent
-  const baseStyle:CSSRuleObject = {
-    backgroundColor: 'var(--surface-bg-color)',
-    color: 'var(--surface-text-color)',
-    borderColor: 'var(--surface-border-color)',
-    'h1,h2,h3,h4,h5,h6': {
-      fontWeight: '600',
-      color: 'var(--surface-heading-color)',
-    },
-    'small,caption,footer': {
-      color: 'var(--surface-caption-color)',
-    },
-    'button': {
-      letterSpacing: '-0.01333em',
-      fontSize: '12px',
-      padding: theme('spacing.1'),
-      paddingLeft: theme('spacing.2'),
-      paddingRight: theme('spacing.2'),
-      borderRadius: theme('borderRadius.sm'),
-      backgroundColor: 'transparent',
-      border: '0px solid var(--btn-border-color)',
-      transition: 'all 0.2s ease-in-out',
-      color: 'var(--txt-color)',
-      '&:hover': {
-        backgroundColor: 'var(--bg-hover)',
-      },
-      '&[type="submit"]': {
-        color: 'var(--surface-btn-bg-color_',
-        backgroundColor: 'var(--bg-color)',
-      },
-    }
+
+  const getColorScale:ColorGetterFn = (hue) => theme(`colors.${hue}`);
+
+  let btnBase:CSSRuleObject = {
+    letterSpacing: '-0.01333em',
+    fontSize: '12px',
+    padding: theme('spacing.1'),
+    paddingLeft: theme('spacing.2'),
+    paddingRight: theme('spacing.2'),
+    borderRadius: theme('borderRadius.sm'),
+    backgroundColor: 'transparent',
+    border: '0px solid var(--border-color)',
+    transition: 'all 0.2s ease-in-out',
+    color: 'var(--txt-color)',
   };
-  // loop over each theme color
-  return shades.map(hue => matchUtilities({
-    [`box-${hue}`]: (value:string) => ({
-      '--tw-ring-opacity': '0.5',
-      '--tw-ring-color': getVar(['700', '200', value], hue, theme),
-      '--tw-border-color': getVar(['600', '300', value], hue, theme),
-      '--surface-heading-color': getVar(['900', '50', value], hue, theme),
-      '--surface-text-color': getVar(['800', '100', value], hue, theme),
-      '--surface-caption-color': getVar(['600', '300', value], hue, theme),
-      '--surface-border-color': getVar(['500', '300', value], hue, theme),
-      '--surface-btn-bg-color': getVar(['50', '300', value], hue, theme),
-      '--surface-bg-color': value,
-      ...baseStyle,
-    })
-  }, {
-    values: theme(`colors.${hue}`),
-    type: 'color',
-  }))
+
+  return !!matchUtilities && Object.keys(theme('colors')).map(hue => {
+    const colors = getColorScale(hue as keyof DefaultColorScales);
+    return matchUtilities({
+
+      [`box-${hue}`]: (value:string) => {
+        let isLight = hexIsLight(value);
+        return {
+          '--tw-ring-color': isLight ? toRGBA(colors['300'], '0.5') : toRGBA(colors['900'], '0.5'),
+          '--tw-ring-opacity': '0.5',
+          '--border-color': toRGBA(isLight ? colors['300'] : colors['500'], '0.8'),
+          backgroundColor: value,
+          color: isLight ? colors['900'] : colors['50'],
+          borderColor: isLight ? colors['500'] : colors['300'],
+          'h1,h2,h3,h4,h5,h6': {
+            fontWeight: '600',
+            color: isLight ? colors['900'] : colors['50'],
+          },
+          'small,caption,footer': {
+            color: isLight ? colors['700'] : colors['300'],
+          },
+          'button': {
+            '--border-color': isLight ? toRGBA(colors[300], '0.5') : toRGBA(colors[900], '0.5'),
+            '--bg-color': toRGBA(`${isLight ? colors[700] : colors[50]}`, '0.75'),
+            '--bg-hover': toRGBA(`${isLight ? colors[700] : colors[50]}`, '0.1'),
+            '--txt-color': isLight ? colors[700] : colors[50],
+            ...btnBase,
+            '&:hover': {
+              backgroundColor: 'var(--bg-hover)',
+            },
+            '&[type="submit"]': {
+              color: isLight ? colors[50] : colors[900],
+              backgroundColor: 'var(--bg-color)',
+            },
+          },
+        };
+      },
+    }, {
+      values: getColorScale(hue as keyof DefaultColorScales),
+      type: 'color',
+    });
+  });
 });
 
 module.exports = Object.assign(surfaces, {
   hexIsLight, toRGBA, getRGBA,
 });
-
